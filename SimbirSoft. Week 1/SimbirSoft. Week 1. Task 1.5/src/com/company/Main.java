7fbfc067b4beeb973165ded6c9670c62f908d839
Vladislav <orlvlav@gmail.com>
2022-05-13 16:16:03 +0400

    .gitignore and task 5

--- SimbirSoft. Week 1/SimbirSoft. Week 1. Task 1.5/src/com/company/Main.java
+++ SimbirSoft. Week 1/SimbirSoft. Week 1. Task 1.5/src/com/company/Main.java
@@ -15,59 +15,32 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите первое число: ");
-        int first = scanner.nextInt();
+        double first = scanner.nextDouble();
         System.out.print("Введите второе число: ");
-        int second = scanner.nextInt();
+        double second = scanner.nextDouble();
         System.out.print("Введите, что Вы хотите сделать с числами (+, -, *, /): ");
         String action = scanner.next();
 
-        scanner.close();
-
-        if ((second == 0) && (action.equals("/")))
+        if (action.equals("+"))
         {
-            System.out.print("Вы патаетесь поделить на ноль. Попробуйте еще раз");
-            System.exit(0);
+            System.out.print("Рузультат сложения числа " + first + " и числа " + second + " равняется " + (first + second));
         }
-
-        switch (action)
+        else if (action.equals("-"))
         {
-            case "+":
-            {
-                System.out.print("Результат сложения " + first + " и " + second + " равен " + (first + second));
-                break;
-            }
-            case "-":
-            {
-                System.out.print("Результат вычитания " + first + " и " + second + " равен " + (first - second));
-                break;
-            }
-            case "*":
-            {
-                System.out.print("Результат умножения " + first + " и " + second + " равен " + (first * second));
-                break;
-            }
-            case "/":
-            {
-                double firstD = first;
-                double secondD = second;
-                if (first < second)
-                {
-                    System.out.print("Результат разности " + first + " и " + second + " равен " + (firstD / secondD));
-                    break;
-                }
-                else if ((first > second) && second % 2 == 0)
-                {
-                    System.out.print("Результат разности " + first + " и " + second + " равен " + (firstD / secondD));
-                    break;
-                }
-                else if ((first > second) && second / 2 != 0)
-                {
-                    System.out.print("Результат разности " + first + " и " + second + " равен " + (firstD / secondD));
-                    break;
-                }
-                System.out.print("Результат разности " + first + " и " + second + " равен " + (first / second));
-                break;
-            }
+            System.out.print("Рузультат вычитания числа " + first + " и числа " + second + " равняется " + (first - second));
+        }
+        else if (action.equals("*"))
+        {
+            System.out.print("Рузультат умножения числа " + first + " и числа " + second + " равняется " + (first * second));
+        }
+        else if (action.equals("/"))
+        {
+            System.out.print("Рузультат деления числа " + first + " и числа " + second + " равняется " + (first / second));
+        }
+        else if (action.equals("/") && (second == 0))
+        {
+            System.out.print("Вы пытаетесь поделить на ноль");
+        }
+        scanner.close();
         }
     }
-}
