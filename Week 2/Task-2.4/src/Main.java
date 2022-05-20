79c89fcfd7e41e15cb428e84bf7da57492d41748
Vladislav <orlvlav@gmail.com>
2022-05-20 17:29:08 +0400

    new task 2.4

--- Week 2/Task-2.4/src/Main.java
+++ Week 2/Task-2.4/src/Main.java
@@ -1,24 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static int number;
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int num = scanner.nextInt();
-        int minToMax = num, maxToMin = num;
-
-        System.out.println("В порядке убывания:");
-        while (maxToMin != -1)
-        {
-            System.out.print(maxToMin-- + " ");
-        }
+        number = getNum();
+        minToMax();
         System.out.println();
-        System.out.println("В порядке возрастания:");
+        maxToMin();
+    }
+    public static int getNum()
+    {
+        System.out.print("Введите число: ");
+        int num;
+
+        if (scanner.hasNextInt())
+        {
+            num = scanner.nextInt();
+        }
+        else
+        {
+            System.out.print("Вы ввели неверное число.");
+            scanner.hasNextInt();
+            num = getNum();
+        }
+        return num;
+    }
+
+    public static int minToMax()
+    {
         int zero = 0;
-        while (zero != minToMax+1)
+        System.out.println("В порядке возрастания:");
+
+        while (zero != number + 1)
         {
             System.out.print(zero++ + " ");
         }
+        return 0;
+    }
+
+    public static int maxToMin()
+    {
+        System.out.println("В порядке уменьшения:");
+        while (number != -1)
+        {
+            System.out.print(number-- + " ");
+        }
+        return 0;
     }
 }
